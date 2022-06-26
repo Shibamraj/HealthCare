ffe83e0814b541503d2a2555f95d4519320d5d23
Shibamraj <shibamraj78@gmail.com>
2022-06-26 23:11:52 +0530

    This is second time update

--- src/main/java/com/example/demo/entity/Specialization.java
+++ src/main/java/com/example/demo/entity/Specialization.java
@@ -1,5 +1,34 @@
 package com.example.demo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="specilization_tab")
 public class Specialization {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="spec_id_col")
+	private long id;
+	
+	@Column(name="spec_code_col")
+	private String code;
+	
+	@Column(name="spec_name_col")
+	private String name;
+	
+	@Column(name="spec_note_col")
+	private String note;
 
 }
