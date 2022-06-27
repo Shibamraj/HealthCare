eac59296ed8f7c5f1d85053ada941d5690c23074
Shibamraj <shibamraj78@gmail.com>
2022-06-27 11:16:50 +0530

    This is Controller,Service and Repository

---
+++ src/main/java/com/example/demo/Controller/SpecializationController.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.Service.SpecializationService;
+
+public class SpecializationController {
+	
+	@Autowired
+	private SpecializationService serv;
+
+}
---
+++ src/main/java/com/example/demo/ServiceImpl/SpecializationServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.ServiceImpl;
+
+import com.example.demo.Service.SpecializationService;
+import com.example.demo.repository.SpecializationRepository;
+
+public class SpecializationServiceImpl implements SpecializationService {
+
+	private SpecializationRepository repo;
+}
